9f8003eb92f87c876a622ba633f181fb3ec03b66
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-08-01 08:49:19 -0300

    Initial Commit

---
+++ FlashZila/FlashZilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZilaApp.swift
+//  FlashZila
+//
+//  Created by Igor Florentino on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashZilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
